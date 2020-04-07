4c49a68b3fa91f8bf995a0f4e5370a9c9c25e41b
min950903 <min950903@gmail.com>
2020-04-07 15:50:04 +0900

    default url 수정

--- src/main/java/com/spirngboot2/aws/webservice/config/auth/SecurityConfig.java
+++ src/main/java/com/spirngboot2/aws/webservice/config/auth/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                         .logoutSuccessUrl("/")
                 .and()
                     .oauth2Login()
+                        .defaultSuccessUrl("/", true)
                             .userInfoEndpoint()
                                 .userService(customOAuth2UserService);
     }
