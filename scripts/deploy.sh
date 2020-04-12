72a65abdf6d0856a9302307e63c9f61047611a83
min950903 <min950903@gmail.com>
2020-04-12 21:20:54 +0900

    current_pid오타수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,11 +8,11 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl spirngboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot2-webservice | grep jar | awk '{print $1}')
 
 echo ">현재 구동죽인 pid : $CURRENT_PID"
 
-if[-z "$CURRENT_PID"];then
+if[-z "$CURRENT_PID"]; then
     echo">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo">kill -15  CURRENT_PID"
