142c2a9971471522eea494eb91da4dcc88a9996f
min950903 <min950903@gmail.com>
2020-04-13 03:32:56 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,9 +10,9 @@ echo ">현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
 
-echo ">현재 구동죽인 pid : $CURRENT_PID"
+echo ">현재 구동죽인 pid : $CURRENT_PID 입니다"
 
-if[-z "$CURRENT_PID"]; then
+if[ -z "$CURRENT_PID"]; then
     echo">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo">kill -15  CURRENT_PID"
