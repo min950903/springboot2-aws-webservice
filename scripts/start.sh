56080e3eb0833891523615ae278f4356492678d7
min950903 <min950903@gmail.com>
2020-04-14 19:35:42 +0900

    배포 스크립트 작성 진행 순서 stop.sh > start.sh > health.sh > switch.sh

---
+++ scripts/start.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/evc bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/ec2/app/step3
+PROJECT_NAME=springboot2-webservice
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name : $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행 권한 추가
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 을 profile=$IDLE_PROFILE 로 실행 합니다."
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.properties,classpath:/appplication-real.properties
+    	, /home/ec2-user/ec2/app/application-oauth.properties
+    	, /hoem/ec2-user/ec2/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+
+
